e97b2a3feb348a5c438cc9c0c1a9e4c4eb9c6036
luisvap <luisvap@gmail.com>
2021-04-20 20:57:51 -0300

    comitao

--- src/main/java/br/uniso/apibd/ApibdApplication.java
+++ src/main/java/br/uniso/apibd/ApibdApplication.java
@@ -2,9 +2,18 @@ package br.uniso.apibd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
-public class ApibdApplication {
+public class ApibdApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApibdApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApibdApplication.class, args);
